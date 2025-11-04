c248625c00bb8046c5b870b0ece73a6de45e65e4
Samadar1 <urapol18@gmail.com>
2025-11-04 23:29:17 +0300

    add: starterpack dependency

---
+++ src/main/java/com/horizen/hc_user_service/HcUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.horizen.hc_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horizen/hc_user_service/HcUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizen.hc_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
